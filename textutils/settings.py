061ac95df2e77e7a800bfe0048e4578717d68828
Ritesh Koshta <riteshnandanwar313@gmail.com>
2022-08-28 08:11:39 +0530

    TextUtils project

--- textutils/settings.py
+++ textutils/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-itk!bd$4huhjwqh8@(yl6#-3ibcz(7ah+bj$@1e#3xhgg8p3f+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
